977eac90c5d66e4037bcd9c726cd65f83800b746
KR0SS0 <103569362+KR0SS0@users.noreply.github.com>
2022-04-17 21:31:12 +0200

    Cauldron updates (Camera + colliders)
    Keeps cauldron in camera, with adjustable variable for how close the cauldron can be to the camera's border.
    
    Also added collision and a trigger for collecting ingredients

--- Potion Pop!/Assets/Scripts/Linus/Cauldron_Movement.cs
+++ Potion Pop!/Assets/Scripts/Linus/Cauldron_Movement.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 public class Cauldron_Movement : MonoBehaviour
 {
     private float cauldronPosY;
+    
+    [Range(-2f, 5f)]
+    [SerializeField] private float cauldronForceCameraBoundsOffset; //How far from camera edge can the cauldron be? 0 = half can be invisible 
+
 
     void Start() 
     { 
-        cauldronPosY = transform.position.y; //Sparar Y-startposition
+        cauldronPosY = transform.position.y; //Saves Y start position
     }
 
     void Update() {
+
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector2(mousePosition.x, cauldronPosY); //Följer datormusens x-led
+        transform.position = new Vector2(mousePosition.x, cauldronPosY); //Cauldron follows cursor's x-pos 
+
+
+        //Force cauldron in camera view
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp(pos.x, (cauldronForceCameraBoundsOffset / 10), 1 - (cauldronForceCameraBoundsOffset / 10));
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+
+       
+
+        //Debug/Testing här vvv
+
     }
 }
