0f3f91c2ac9acb3a35d4a7a0711f655ef22f044c
isabelmedettl <isabel.d.johansson@gmail.com>
2022-04-19 12:18:37 +0200

    Kopiera Linus filer

---
+++ Potion Pop!/Assets/Scripts/Isabel/Ingredient copy.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ingredient : MonoBehaviour
+{
+
+    private void Start() {
+
+    }
+
+    void Update()
+    {
+
+    }
+    
+}
