272c9c063160499e1a30bf2b5703207b9d60a24c
isabelmedettl <isabel.d.johansson@gmail.com>
2022-04-19 20:15:57 +0200

    bara skit, jobbat i annat prrojekt

---
+++ Potion Pop!/Assets/Scripts/Isabel/QuestHandler.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//LINUS:
+// spawn
+// ingredient counter ui
+
+//ISABEL:
+// Questhandler
+// quest i qauldron
+// questSystem
+
+public class QuestHandler : MonoBehaviour
+{
+   
+}
