81edcecdf691156fb8170b939ea99f3aa65bf118
Linus Ekberg <103569362+KR0SS0@users.noreply.github.com>
2022-04-20 15:31:55 +0200

    Uppdaterade rotate script

--- Potion Pop!/Assets/Scripts/Rotate.cs
+++ Potion Pop!/Assets/Scripts/Rotate.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-    public float speed = 100.0f;
+
+    public float minSpeed = 40f;
+    public float maxSpeed = 150f;
+
+    private bool rotatesClockwise;
+    private float speed;
+
+    void Start() {
+
+        //Väljer randomly hur snabbt objektet ska rotera
+        speed = Random.Range(minSpeed, maxSpeed);
+        
+        //Väljer randomly vilket håll objektet ska rotera
+        int temp = Random.Range(1, 10);
+        if(temp % 2 == 0) {
+            rotatesClockwise = false;
+        } else {
+            rotatesClockwise = true;
+        }
+    }
 
     void Update()
     {
-        transform.Rotate(Vector3.forward * Time.deltaTime * speed);
+        if(rotatesClockwise) {
+            transform.Rotate(Vector3.forward * Time.deltaTime * speed);
+        } else {
+            transform.Rotate(Vector3.forward * Time.deltaTime * (speed * -1));
+        }
+        
     }
 }
