f54736b372d92d68d94a1174ab78d125cb409cf4
Linus Ekberg <ekberglinus01@gmail.com>
2022-04-21 17:36:36 +0200

    Frenzy spawn system
    Målet just nu är att få spawning av ingredienser att vara mer spännande och mindre repetitivt!

--- Potion Pop!/Assets/Scripts/Emitter.cs
+++ Potion Pop!/Assets/Scripts/Emitter.cs
@@ -11,9 +11,26 @@ public class Emitter : MonoBehaviour {
     private Transform spawnPos;
     private float timer;
 
+    private float spawnTime;
+    public float minSpawnTime;
+    public float maxSpawnTime;
+
+    public float chanceForSpawnFrenzy;
+    public float spawnFrenzyTime = 3f;
+    public float spawnFrenzyAmount = 10f;
+    private float spawnFrenzyCount = 0f;
+    private bool isFrenzy = false;
+
+
     // Start is called before the first frame update
     void Start() {
         spawnPos = GetComponent<Transform>();
+
+        spawnTime = minSpawnTime;
+
+        if (spawnFrenzyTime == 0) {
+            spawnFrenzyTime = minSpawnTime / 2;
+        }
 
         //Debug feedback
         if (level.ingredientsToSpawn[0] == null) {
@@ -24,12 +41,30 @@ public class Emitter : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-
         timer += Time.deltaTime;
-        if (timer > 1f && level.ingredientsToSpawn[0] != null) {
+        if (timer > spawnTime && level.ingredientsToSpawn[0] != null && !isFrenzy) {
             Instantiate(level.ingredientsToSpawn[Random.Range(0, level.ingredientsToSpawn.Length)], spawnPos.position, spawnPos.rotation);
+
+            spawnTime = Random.Range(minSpawnTime, maxSpawnTime);
+            Debug.Log("Test"); 
+            
+
             timer = 0f;
+            if (chanceForSpawnFrenzy >= Random.Range(0,100)) {
+                isFrenzy = true;
+                Debug.Log("frenzy!");
+            }
         }
 
+        if(isFrenzy && (timer > spawnFrenzyTime)) {
+            Instantiate(level.ingredientsToSpawn[Random.Range(0, level.ingredientsToSpawn.Length)], spawnPos.position, spawnPos.rotation);
+            spawnFrenzyCount++;
+            timer = 0f;
+            if (spawnFrenzyCount >= spawnFrenzyAmount) {
+                isFrenzy = false;
+                spawnFrenzyCount = 0;
+                Debug.Log("not frenzy!");
+            }
+        }
     }
 }
