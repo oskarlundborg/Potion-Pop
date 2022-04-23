9c3c04776ec6324f1f97115a9aeb6d2860db207b
Fenster <oskarlundborg@hotmail.com>
2022-04-23 14:17:08 +0200

    Basic Level Loader Created

---
+++ Potion Pop!/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [SerializeField] private Animator transition;
+    [SerializeField] private float transitionTime = 1f;
+
+    
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+         
+    }
+}
