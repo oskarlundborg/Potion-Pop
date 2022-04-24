3dc1e227617c788bc9f1b96994e3e58ce0255ff7
Fenster <oskarlundborg@hotmail.com>
2022-04-24 12:51:31 +0200

    Updated main menu with level loader

--- Potion Pop!/Assets/Scripts/LevelLoader.cs
+++ Potion Pop!/Assets/Scripts/LevelLoader.cs
@@ -8,10 +8,20 @@ public class LevelLoader : MonoBehaviour
     [SerializeField] private Animator transition;
     [SerializeField] private float transitionTime = 1f;
 
-    
+
     public void LoadNextLevel()
     {
         StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    public void LoadMainMenu()
+    {
+        StartCoroutine(LoadLevel(0));
+    }
+
+    public void LoadSpecificLevel(int i)
+    {
+        StartCoroutine(LoadLevel(i));
     }
 
     IEnumerator LoadLevel(int levelIndex)
