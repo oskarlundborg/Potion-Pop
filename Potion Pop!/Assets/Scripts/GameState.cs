b367063e74d906aed9ac71717a5709d501d8fc59
Fenster <oskarlundborg@hotmail.com>
2022-05-14 14:24:30 +0200

    GameState updated

--- Potion Pop!/Assets/Scripts/GameState.cs
+++ Potion Pop!/Assets/Scripts/GameState.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class GameState : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private LevelData[] levelDataArray = new LevelData[9];
+    private GameData gameData;
+    private int totalAmountOfStars;
+    private int latestUnlockedLevel;
+
     void Start()
     {
-        
+        ImportLevelData();
+        //Get total number of unlocked stars
+        //Get latest unlocked level number
     }
 
-    // Update is called once per frame
-    void Update()
+    private void ImportLevelData()
     {
-        
+        for(int i = 0; i < levelDataArray.Length; i++)
+        {
+            levelDataArray[i] = SaveSystem.LoadLevel(i + 1);
+        }
     }
+
+    private void ImportGameData()
+    {
+
+    }
+
+    private void CalculateStars()
+    {
+
+    }
+
+    public int GetTotalStars()
+    {
+        return totalAmountOfStars;
+    }
+
+    public int GetLatestUnlockedLevel()
+    {
+        return latestUnlockedLevel;
+    }
+
+
 }
