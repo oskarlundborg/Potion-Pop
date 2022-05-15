1208be337d00115af87af0134b82774e6ec9adea
Isabel <isabel.d.johansson@gmail.com>
2022-05-15 13:13:43 +0200

    Auto stash before rebase of "origin/main"

--- Potion Pop!/Assets/Scripts/GameData.cs
+++ Potion Pop!/Assets/Scripts/GameData.cs
@@ -2,18 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//kanske ta bort
-public class GameData : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+[System.Serializable]
+public class GameData
+{
+    private int lastUnlockedLevel;
+    private int totalAmountOfStars;
+
+    public GameData(GameState gameState) 
     {
-        
+        lastUnlockedLevel = gameState.GetLastUnlockedLevel();
+        totalAmountOfStars = gameState.GetTotalStars();
+    }
+   
+    public int GetLastLevelUnlocked() 
+    {
+        return lastUnlockedLevel;
+    }
+    public int GetTotalAmountOfStars() 
+    {
+        return totalAmountOfStars;
     }
 }
--- Potion Pop!/Assets/Scripts/LevelData.cs
+++ Potion Pop!/Assets/Scripts/LevelData.cs
@@ -3,31 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-// FÖR SAVE-Files
-// stjärnor per level
 
 [System.Serializable]
 public class LevelData 
 {
-    public int starsUnlocked;
-    public int highScore;
+    private bool isLastLevel;
+    private int starsUnlocked;
+    private int highScore;
 
     public LevelData(LevelState levelState) {
         highScore = levelState.GetLevelHighScore();
         starsUnlocked = levelState.GetStarsUnlocked();
     }
+    public int GetStarsUnlocked() {
+        return starsUnlocked;
+    }
+    public int GetHighScore() {
+        return highScore;   
+    }
+    
+    public bool GetIsLastLevel()
+    {
+        return isLastLevel;
+    }
+
+    public void SetIsLastLevel() 
+    {
+        isLastLevel = !isLastLevel;
+    }
+
 }
-
-/*
- * Vi ska spara:
- * - highscore för varje level
- * - antal vunna stjärnor för varje level
- * - am i the latest played level
- * 
- * Ett annat script som har:
- * - totala vunna stjärnor för alla spelade levels 
- * - senast spelade level
- * 
- */
-
 
