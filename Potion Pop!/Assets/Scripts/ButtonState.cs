66a6994ffae3cf2219df96d0f85222dac73ae267
Agnes Sandberg <agnespaulina@live.se>
2022-05-21 14:01:04 +0200

    Funktionella knappar
    options, menu, levels

--- Potion Pop!/Assets/Scripts/ButtonState.cs
+++ Potion Pop!/Assets/Scripts/ButtonState.cs
@@ -6,21 +6,41 @@ using UnityEngine.UI;
 public class ButtonState : MonoBehaviour
 {
     bool isAwake;
-    public Button button;
-    public Image ImageButton;
+    private Button button;
+    
     public Sprite off;
     public Sprite on;
 
 
+    
 
     public void changeText()
     {
         
-        Text txt = transform.Find("Text").GetComponent<Text>();        
-        txt.text = isAwake ? "ON" : "OFF";
-        button.image.sprite = isAwake ? on : off;
-        isAwake = !isAwake;
+        //Text txt = transform.Find("Text").GetComponent<Text>();        
+        //txt.text = isAwake ? "ON" : "OFF";
+        //button.image.sprite = isAwake ? on : off;
+        //isAwake = !isAwake;
 
     }
+
+    public void TurnOff(Button b)
+           
+    {
+        if (b.image.sprite == off)
+        {
+            b.image.sprite = on;
+
+        }
+        else
+        {
+            b.image.sprite = off;
+         
+        }
+    
+
+    }
+
+
 
 }
