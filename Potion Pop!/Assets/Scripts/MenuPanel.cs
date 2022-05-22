11b15f1aa3df9d1d634239c02e7a0304904b31a5
agnespaulina <agnessandberg@live.se>
2022-05-22 23:34:04 +0200

    Revert "Revert "Merge pull request #130 from oskarlundborg/agnesfix2""
    This reverts commit 6c7e2e5d0f83abecda250496069c03a1bb88c0ea.

--- Potion Pop!/Assets/Scripts/MenuPanel.cs
+++ Potion Pop!/Assets/Scripts/MenuPanel.cs
@@ -8,10 +8,10 @@ public class MenuPanel : MonoBehaviour
     [SerializeField] private LevelLoader levelLoader;
     [SerializeField] private GameObject menuPopup;
 
-    // Start is called before the first frame update
+ 
     void Start()
     {
-        menuPopup.gameObject.SetActive(false);
+        ClosePopup();
     }
 
     public void OpenPopup()
